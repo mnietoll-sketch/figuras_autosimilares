709667c8330d9ee8596c3fc56d466d9fe0671927
Mauricio <mnietoll@unsa.edu.pe>
2026-05-27 15:37:55 -0500

    agregar fractalTree examples/testline.c

--- examples/testLine.c
+++ examples/testLine.c
@@ -15,28 +15,8 @@ int main(void){
 
 	turtleSetColor(t, 255, 100, 0);
 	turtleSetSpeed(t, 10.0f);
-	//square(t, 100.0f);
-	//turtleForward(t, 100.0f);
-	//splitLine(t, 2000.0f, 10, 10, 255.0f, 255.0f, 255.0f);
-
-	long int* count;
-	float *r, *g, *b;
 	
-	count = (long int*)malloc(sizeof(long int));
-	r = (float*)malloc(sizeof(float));
-	g = (float*)malloc(sizeof(float));
-	b = (float*)malloc(sizeof(float));
-	
-	*count = 0, *r = 0.0f, *g = 255.0f, *b = 255.0f;
-	for(int i = 0; i < 3; i++){
-		koch(t, 400.0f, 5, 5, count, r, g, b);
-		turtleRight(t, 120.0f);	
-	}
-
-	free(count);
-	free(r);
-	free(g);
-	free(b);
+	fractalTree(t, 50.0f, 2);
 
 	turtleAppRun(app);
 	turtleAppDestroy(app);
