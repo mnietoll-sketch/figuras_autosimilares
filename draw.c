09520267afd1b6a3b06b60787cb3bf92a2e614a0
Mauricio <mnietoll@unsa.edu.oe>
2026-05-28 00:34:21 -0500

    creada variacion propia, struct color y funcion color_sub

--- draw.c
+++ draw.c
@@ -1,6 +1,14 @@
 #include "draw.h"
 #include "turtlec.h"
 
+struct color color_sub(struct color to, struct color from){
+	struct color result;
+
+	result.r = to.r - from.r;
+	result.g = to.g - from.g;
+	result.b = to.b - from.b;
+}
+
 void fractalTree(Turtle *turtle, float length, int depth, float reduction, float angle){
 	if(depth <= 0 || length < 5.0f)
 		return;
@@ -19,7 +27,24 @@ void fractalTree(Turtle *turtle, float length, int depth, float reduction, float
 	if(depth >= 5)
 		turtleSetColor(turtle, 150, 75, 0);
 
-
 	turtleLeft(turtle, angle);
+	turtleBackward(turtle, length);
+}
+
+void fractalTreeVariation(Turtle *turtle, float length, int depth, struct color from, struct color to){
+	if(depth <= 0 || length < 5.0f)
+		return;
+
+	struct color grad = color_sub(to - from);
+
+	turtleForward(turtle, length);
+	
+	turtleLeft(turtle, 30);
+	fractalTree(turtle, length * 0.7, depth - 1);
+	
+	turtleRight(turtle, 60);
+	fractalTree(turtle, length * 0.7, depth - 1);
+	
+	turtleLeft(turtle, 30);
 	turtleBackward(turtle, length);
 }
--- draw.h
+++ draw.h
@@ -3,6 +3,16 @@
 
 #include "turtlec.h"
 
+struct color{
+	float r;
+	float g;
+	float b;
+}
+
+struct color color_sub(struct color to, struct color from);
+
 void fractalTree(Turtle *turtle, float length, int depth, float reduction, float angle);
+
+void fractalTreeVariation(Turtle *turtle, float length, int depth, struct color from, struct color to);
 
 #endif
