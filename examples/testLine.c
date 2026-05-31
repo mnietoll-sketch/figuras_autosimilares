02fc0322b7d0a2fdb71ed030cb554eec29099d53
Mauricio <mnietoll@unsa.edu.oe>
2026-05-31 04:01:33 -0500

    Se realizaron algunas ejecuciones para conseguir evidencia para escribir el informe

--- examples/testLine.c
+++ examples/testLine.c
@@ -16,28 +16,27 @@ int main(void){
 	turtleSetColor(t, 255, 150, 0);
 	turtleSetSpeed(t, 10.0f);
 	
-	fractalTree(t, 100.0f, 10);
-	//fractalTreeGeneralized(t, 100.0f, 10, 0.8f, 30);
+	//fractalTree(t, 100.0f, 10);
+	//fractalTreeGeneralized(t, 150.0f, 12, 0.7f, 30.0f);
 	
 	/*
 	struct color from, to;
 	from = color_init(150.0f, 75.0f, 0.0f);
 	to = color_init(0.0f, 255.0f, 0.0f);
-	fractalTreeVariation(t, 100.0f, 10, from, to);
+	fractalTreeVariation(t, 150.0f, 12, from, to);
 	*/
 	
-	/*
-	levy(t, 200.0f, 6);
-	*/
+	
+	//levy(t, 200.0f, 10);
 	/*
 	struct color from, to;
 	from = color_init(255.0f, 0.0f, 0.0f);
 	to = color_init(0.0f, 255.0f, 255.0f);
 	levyVariation(t, 300.0f, 10, from, to);
 	*/
-	/*
+	
 	fractalTreeChallenge(t, 200.0f, 8, 0.6f);
-	*/
+	
 	turtleAppRun(app);
 	turtleAppDestroy(app);
 	return 0;
